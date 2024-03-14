b4d2be34b99d3127111970623e57edbb3425fb7d
MLCH <mlch@eaaa.dk>
2024-03-14 21:47:23 +0100

    Upload gui 1 demoes

---
+++ L18/src/guidemo_checkbox_1/App.java
@@ -0,0 +1,9 @@
+package guidemo_checkbox_1;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(CheckBoxDemo1.class);
+    }
+}
