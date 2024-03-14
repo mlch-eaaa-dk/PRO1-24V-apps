bd4b1cd38238aaa36bc60a33dd85de906ba1f98a
MLCH <mlch@eaaa.dk>
2024-03-14 15:54:58 +0100

    Initialize repo

---
+++ L18/src/guidemo_1/App.java
@@ -0,0 +1,9 @@
+package guidemo_1;
+
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(GuiDemo1.class);
+    }
+}
